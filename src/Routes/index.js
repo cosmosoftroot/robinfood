f4f000c83cf6e6ffff0549017422945bfc989b56
Ivan D. Sanchez <cosmosoft.soluciones@gmail.com>
2022-02-28 22:13:49 -0500

    Add basic styles

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,13 +1,17 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Layout from "../components/Layout"
+import Login from "../pages/Login"
 
 export const Router = () => {
   return(
     <BrowserRouter>
+    <Layout>
       <Routes>
         <Route path={"/tiendas"}  element={<div>test</div>}/>
         <Route path={"/tienda/:id"}  element={<div>tienda</div>}/>
-        <Route path={"/"}  element={<div>login</div>}/>
+        <Route path={"/"}  element={<Login />}/>
       </Routes>
+    </Layout>
     </BrowserRouter>
   )
 }
